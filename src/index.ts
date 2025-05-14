2210baddc887aa22401e257fc494dfa43005cd8f
bholagabbar <11693595+bholagabbar@users.noreply.github.com>
2025-05-13 23:46:49 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,7 @@
  */
 
 import { Server } from "@modelcontextprotocol/sdk/server/index.js";
--import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-+import { HttpServerTransport } from "@modelcontextprotocol/sdk/server/http.js";
+import { HttpServerTransport } from "@modelcontextprotocol/sdk/server/http.js";
 import {
   ListResourcesRequestSchema,
   ReadResourceRequestSchema,
